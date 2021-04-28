9b4c3155eefca1587860ade91898db6ee018e213
ananay <ananay.narang@gmail.com>
2021-04-28 19:47:47 +0530

    update

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -5,4 +5,20 @@ pragma solidity ^0.5.16;
  */
 contract ToDoList {
   uint public taskCount = 0;
+struct Task {
+   uint id;
+   string content;
+   bool completed;
+}
+
+	constructor() public {
+		createTask("test task ignore");
+	}
+  mapping(uint => Task) public tasks;
+
+  function createTask(string memory _content) public {
+  	taskCount ++;
+	tasks[taskCount] = Task(taskCount, _content, false);  	
+  }
+  
 }
